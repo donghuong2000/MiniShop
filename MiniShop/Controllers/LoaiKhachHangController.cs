ff0dcb5ca57f625f6209f827d5309dcb283f85c7
Nguyen Dong Huong <donghuong2000@users.noreply.github.com>
2020-11-16 10:14:11 +0700

    add view index for loaikh

---
+++ MiniShop/Controllers/LoaiKhachHangController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using MiniShop.Exten;
+using MiniShop.Repository.IRepository;
+
+namespace MiniShop.Controllers
+{
+    public class LoaiKhachHangController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public LoaiKhachHangController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetAll()
+        {
+            var result = _unitOfWork.SP_Call.Excute(SD.Loai_Khach_Hang.GET_ALL);
+            if(result.success)
+            {
+                return Content(result.message, "application/json");
+            }
+            return NotFound();
+        }
+        public IActionResult Get(string id)
+        {
+            var parameter = new DynamicParameters();
+            parameter.Add("@MALOAIKH", id);
+            var result = _unitOfWork.SP_Call.Excute(SD.Loai_Khach_Hang.GET,parameter);
+            if (result.success)
+            {
+                return Content(result.message, "application/json");
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ MiniShop/wwwroot/js/loaikhachhang.js
@@ -0,0 +1,148 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$(document).ready(function () {
+    $('#dataTable').DataTable({
+        "ajax": {
+            "url": "/loaikhachhang/GetAll"
+        },
+        "columns": [
+            { "data": "MALOAIKH" },
+            { "data": "TENLOAI" },
+            {
+                "data": "MALOAIKH",
+                "render": function (data) {
+                    return `
+                             <div class="text-center">
+                                <a data-toggle="modal" data-target="#EditModal" data-whatever="${data}" class="btn btn-success text-white" style="cursor:pointer">
+                                    <i class="fas fa-edit"></i>
+                                </a>
+                                <a onClick=Delete("/loaikhachhang/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer">
+                                    <i class="fas fa-trash-alt"></i>
+                                </a>
+                            </div>                           
+                            
+                           `;
+                }
+  
+            }
+             
+        ]
+
+    });
+});
+
+const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+    },
+    buttonsStyling: false
+})
+function Delete(url) {
+    swalWithBootstrapButtons.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Yes, delete it!',
+        cancelButtonText: 'No, cancel!',
+        reverseButtons: true
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        swalWithBootstrapButtons.fire(
+                            'Deleted!',
+                            'Your file has been deleted.',
+                            'success'
+                        );
+                        $('#dataTable').DataTable().ajax.reload();
+                    }
+                    else {
+                        swalWithBootstrapButtons.fire(
+                            'Error',
+                            'Can not delete this, maybe it not exit or error from sever',
+                            'error'
+                        )
+                    }
+                }
+
+            })
+
+        }
+        else if (result.dismiss === Swal.DismissReason.cancel) {
+            swalWithBootstrapButtons.fire(
+                'Cancelled',
+                'Your record is safe :)',
+                'error'
+            )
+        }
+    })
+}
+
+
+
+$('#EditModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget) // Button that triggered the modal
+    var idname = button.data('whatever') // Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    var modal = $(this)
+    $.ajax({
+        method: 'GET',
+        url: '/loaikhachhang/get/' + idname,
+        success: function (data) {
+           
+            modal.find('#ma_nhan_vien').val(data.data[0].MANV)
+            modal.find('#ma_nhan_vien_old').val(data.data[0].MANV)
+            modal.find('#ten_nhan_vien').val(data.data[0].TENNV)
+            modal.find('#ngay_sinh').val(data.data[0].NGAYSINH)
+            modal.find('#gioi_tinh').val(data.data[0].GIOITINH)
+            modal.find('#cmnd').val(data.data[0].CMND)
+            modal.find('#sdt').val(data.data[0].SDT)
+            modal.find('#chuc_vu').val(data.data[0].CHUCVU)
+            modal.find('#ngay_lam_viec').val(data.data[0].NGAYLAMVIEC)
+            modal.find('#dia_chi').val(data.data[0].DIACHI)
+            modal.find('#user_name').val(data.data[0].USERNAME)
+        }
+    })
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    
+    
+})
+
+$('#updateform').click(function () {
+    var a = $('#ma_nhan_vien').val()
+    var b = $('#ma_nhan_vien_old').val()
+    var c = $('#ten_nhan_vien').val()
+    var d = $('#ngay_sinh').val()
+    var e = $('#gioi_tinh').val()
+    var f = $('#cmnd').val()
+    var g = $('#sdt').val()
+    var h = $('#chuc_vu').val()
+    var k = $('#dia_chi').val()
+    var l = $('#user_name').val()
+    $.ajax({
+        method: 'POST',
+        data: {a:a,b:b,c:c,d:d,e:e,f:f,g:g,h:h,k:k,l:l},
+        url: '/nhanvien/update/',
+        contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+        success: function (data) {
+            if (data.success) {
+                toastr.success(data.message);
+                $('#dataTable').DataTable().ajax.reload();
+            }
+            else {
+
+                toastr.error(data.message);
+            }
+        }
+    })
+
+}
+)
